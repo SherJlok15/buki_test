a3d68a48ac4716225b3750caf4ff4a6882e0e06d
Fandorin <SherJlok15@gmail.com>
2019-09-02 11:21:48 +0300

    Updates

---
+++ precache-manifest.17e8005d8a428b09d84d4acee05fbd79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30487cd3df616342c4cd597a2e15d0f",
+    "url": "/buki_test/index.html"
+  },
+  {
+    "revision": "1496b05babe08aba97ef",
+    "url": "/buki_test/static/css/main.0067d81d.chunk.css"
+  },
+  {
+    "revision": "71fb7b388d0948c6b682",
+    "url": "/buki_test/static/js/2.8b2a4481.chunk.js"
+  },
+  {
+    "revision": "1496b05babe08aba97ef",
+    "url": "/buki_test/static/js/main.d05ce7b3.chunk.js"
+  },
+  {
+    "revision": "be9b576b20d96cf89909",
+    "url": "/buki_test/static/js/runtime~main.0a24696d.js"
+  },
+  {
+    "revision": "07d53b5b4d43b671764d7075be494019",
+    "url": "/buki_test/static/media/Arial.07d53b5b.ttf"
+  }
+]);
